151c7dba8a54046ca9d91c92f1c6a90afe9ec169
SXY <sunxiaoyu1115@gmail.com>
2026-02-20 10:49:18 +0800

    Update basic_transformer_models.py

--- basic_transformer_models.py
+++ basic_transformer_models.py
@@ -60,16 +60,13 @@ class FeedForward(nn.Module):
         self.cnn1 = nn.Conv1d(d_model, d_ff, kernel_size=1)
         self.cnn2 = nn.Conv1d(d_ff, d_model, kernel_size=1)
         self.act = nn.ReLU()
-        # self.linear1 = nn.Linear(d_model, d_ff)
-        # self.linear2 = nn.Linear(d_ff, d_model)
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.linear2 = nn.Linear(d_ff, d_model)
         
     def forward(self, x):
         x = self.act(self.cnn1(x.permute(0, 2, 1)))
         x = self.cnn2(x)
         x = x.permute(0, 2, 1)
-        # x = self.linear1(x)
-        # x = self.act(x)
-        # x = self.linear2(x)
         return x
 
 
@@ -95,9 +92,6 @@ class DecoderLayer(nn.Module):
         self.self_attn = MultiHeadAttention(d_model, num_heads)
         self.cross_attn = MultiHeadAttention(d_model, num_heads)
         self.ffn = FeedForward(d_model, d_ff)
-        # self.norm1 = nn.LayerNorm(d_model)
-        # self.norm2 = nn.LayerNorm(d_model)
-        # self.norm3 = nn.LayerNorm(d_model)
         self.norm1 = nn.BatchNorm1d(d_model)
         self.norm2 = nn.BatchNorm1d(d_model)
         self.norm3 = nn.BatchNorm1d(d_model)
@@ -153,10 +147,6 @@ class DilatedCausalCNNMapper(nn.Module):
         self.causal_layers = nn.Sequential(*layers)
 
     def forward(self, x, target_seq_len):
-        """
-        x: [batch_size, seq_len+pred_horizon, d_model]
-        returns: [batch_size, seq_len, d_model] causally summarized
-        """
         x = x.transpose(1, 2)
         x = self.causal_layers(x)
         x = x[:, :, -target_seq_len:]
@@ -177,7 +167,7 @@ class TeacherEncoder(nn.Module):
     def forward(self, x, key_padding_mask=None):
         x = self.pos_enc(self.input_fc1(x), start_pos=0)
         for layer in self.layers:
-            x = layer(x) #, mask=mask, key_padding_mask=key_padding_mask)
+            x = layer(x)
         return x
 
 
@@ -193,14 +183,9 @@ class SharedDecoder1(nn.Module):
         self.linear3 = nn.Linear(32, 1)
 
     def forward(self, mem):
-        if self.avg_pool:
-            x = mem.mean(dim=1)
-        else:
-            x = mem[:,-1,:]
+        x = mem[:,-1,:]
         x = self.linear1(x)
-        # x = self.act1(x)
         x = self.linear2(x)
-        # x = self.act2(x)
         x = self.linear3(x)
         return x.squeeze(-1)
 
@@ -234,65 +219,8 @@ class Transformer_seq2seq_model(nn.Module):
     def forward(self, x):
         enc_out = self.encoder_layers(x)
         x = enc_out.mean(dim=1)
-        # repeated_h = enc_out.unsqueeze(1).repeat(1, self.pred_horizon, 1)
-        # repeated_h, _ = self.decoder_lstm(repeated_h)
         out = self.decoder(x)
         return out.squeeze()
-
-
-class  ConcatCausalSelfAttention(nn.Module):
-    def __init__(self, d_model, n_head, num_layers=1):
-        super(ConcatCausalSelfAttention, self).__init__()
-        self.layers = nn.ModuleList([
-            nn.TransformerEncoderLayer(d_model=d_model, nhead=n_head, batch_first=True) for _ in range(num_layers)
-        ])
-
-    def forward(self, input1, input2):
-        """
-        input1: [B, seq_len, d_model]
-        input2: [B, pred_horizon, d_model]
-        return fused features for input2: [B, pred_horizon, d_model]
-        """
-        B, seq_len, d_model = input1.shape
-        pred_horizon = input2.shape[1]
-        x = torch.cat([input1, input2], dim=1)
-        total_len = seq_len + pred_horizon
-
-        attn_mask = torch.zeros(total_len, total_len, device=x.device, dtype=torch.bool)
-
-        # for input2 part, mask future tokens
-        for i in range(pred_horizon):
-            idx = seq_len + i
-            attn_mask[idx, seq_len + i + 1:] = True
-
-        # Pass through Transformer encoder layers
-        for layer in self.layers:
-            x = layer(x, src_mask=attn_mask)
-        fused_input2 = x[:, seq_len:, :]
-        return fused_input2
-
-class FutureSelfAttention(nn.Module):
-    def __init__(self, d_model, n_head, pred_horizon=12):
-        super(FutureSelfAttention, self).__init__()
-        self.pred_horizon = pred_horizon
-        self.d_model = d_model
-        self.future_queries = nn.Parameter(torch.randn(pred_horizon, d_model))
-        self.attn = nn.MultiheadAttention(d_model, n_head, batch_first=True)
-        self.ffn = nn.Sequential(nn.Linear(d_model, 4*d_model),
-                                 nn.ReLU(),
-                                 nn.Linear(4*d_model, d_model))
-        self.norm = nn.LayerNorm(d_model)
-        self.proj = nn.Linear(2*d_model, d_model)
-
-    def forward(self, input1, input2):
-        B, seq, _ = input1.shape
-        queries = self.future_queries.unsqueeze(0).repeat(B, 1, 1)
-        keys_values = input1
-        out, _ = self.attn(queries, keys_values, keys_values)
-        out = self.norm(out)
-        x = torch.cat((out, input2), dim=-1)
-        out = self.proj(x)
-        return out
 
 
 
@@ -304,7 +232,7 @@ class Transformer_seq2seq_Teacher_model(nn.Module):
         self.encoder1 = TeacherEncoder(channels=channels, d_model=d_model, n_heads=num_heads, num_layers=num_enc_layer, d_ff=d_ff)
         self.encoder2 = TeacherEncoder(channels=channels-1, d_model=d_model, n_heads=num_heads, num_layers=num_enc_layer,
                                        d_ff=d_ff)
-        # self.fusion = ConcatCausalSelfAttention(d_model=d_model, n_head=num_heads)
+        self.fusion = ConcatCausalSelfAttention(d_model=d_model, n_head=num_heads)
         self.fusion = FutureSelfAttention(d_model=d_model, n_head=num_heads, pred_horizon=pred_horizon)
         self.pred_horizon = pred_horizon
         self.decoder = nn.Sequential(nn.Linear(d_model, 256),
@@ -329,19 +257,13 @@ class Transformer_seq2seq_Encoder_model(nn.Module):
         self.encoder1 = TeacherEncoder(channels=channels, d_model=d_model, n_heads=num_heads, num_layers=num_enc_layer, d_ff=d_ff)
         self.encoder2 = TeacherEncoder(channels=channels-1, d_model=d_model, n_heads=num_heads, num_layers=num_enc_layer,
                                        d_ff=d_ff)
-        # self.map = nn.Sequential(nn.Linear(d_model*2, d_model))
+        self.map = nn.Sequential(nn.Linear(d_model*2, d_model))
         self.pred_horizon = pred_horizon
         self.fusion = FutureSelfAttention(d_model=d_model, n_head=num_heads, pred_horizon=pred_horizon)
 
     def forward(self, x):
         x1 = x[:, :-self.pred_horizon]
         x2 = x[:, -self.pred_horizon:, 1:]
-        # enc_out1 = self.encoder1(x1)
-        # x1 = enc_out1.mean(dim=1)
-        # x2 = self.encoder2(x2)
-        # x2 = x2.mean(dim=1)
-        # fused = torch.cat([x1, x2], dim=-1)
-        # out = self.map(fused)
         enc1 = self.encoder1(x1)
         enc2 = self.encoder2(x2)
         out = self.fusion(enc1, enc2)
@@ -365,13 +287,11 @@ class Transformer_seq2seq_Student_Encoder_model(nn.Module):
                  num_enc_layer=2, d_ff=128):
         super(Transformer_seq2seq_Student_Encoder_model, self).__init__()
         self.encoder = TeacherEncoder(channels=channels, d_model=d_model, n_heads=num_heads, num_layers=num_enc_layer, d_ff=d_ff)
-        # self.map = nn.Sequential(nn.Linear(d_model, 4*d_model), nn.ReLU(), nn.Linear(4*d_model, seq_len))
         self.map = nn.Sequential(nn.Linear(seq_len, d_ff), nn.Linear(d_ff, pred_horizon))
 
     def forward(self, x):
         enc_out = self.encoder(x)
-        # out = enc_out.mean(dim=1)
-        # out = out + self.map(out)
         out = self.map(enc_out.transpose(1, 2)).transpose(1, 2)
         return out.squeeze()
+
 
